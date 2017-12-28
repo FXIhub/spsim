8c14b647f2933d711dc0ca5bc8287d563d2a9a6a
Benedikt J. Daurer <bjdaurer@gmail.com>
2017-12-28 16:36:56 +0100

    Made import statements Python 3 compatible

--- src/spsim.py
+++ src/spsim.py
@@ -7,8 +7,11 @@ from spsim_pybackend import *
 
 import numpy
 
-import urllib2
-import StringIO
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+from io import StringIO
 import gzip
 
 # Convenience functions
@@ -39,7 +42,7 @@ def get_atoms_from_molecule(mol):
 def fetch_pdb(pdb_id):
     url = "http://www.rcsb.org/pdb/files/%s.pdb.gz" % str(pdb_id)
     filename = "./%s.pdb" % str(pdb_id)
-    response = urllib2.urlopen(url)
+    response = urlopen(url)
     compressedFile = StringIO.StringIO()
     compressedFile.write(response.read())
     compressedFile.seek(0)
