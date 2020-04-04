047e7480177e861e85ef47b02d16d8922f6bee49
Filipe Maia <filipe.c.maia@gmail.com>
2020-04-05 00:53:10 +0200

    Add missing header

--- include/io.h
+++ include/io.h
@@ -17,6 +17,7 @@
  *
  */
 
+#include "config.h"
 
 void write_2D_array_to_vtk(float * f,int nx, int ny, char * filename);
 void write_3D_array_to_vtk(float * f,int nx, int ny,int nz, char * filename);
