9da494d257ed549b1bd6e738fa9a3b6cc9ec4ea7
Filipe Maia <filipe.c.maia@gmail.com>
2020-04-04 23:35:23 +0200

    Hide the fancy sse header on windows

--- src/diffraction.c
+++ src/diffraction.c
@@ -44,7 +44,9 @@
 #include "mpi_comm.h"
 #include "io.h"
 #include "box.h"
+#if defined __GNUC__ && !defined __clang__
 #include "sse_mathfun.h"
+#endif
 
 #ifndef M_PI
 #define M_PI     3.1415926535897932384626433832795029L
